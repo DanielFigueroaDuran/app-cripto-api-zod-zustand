256782d9c78dcbe7fa408d5f9e2059ed49bfa739
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-18 18:25:56 +0200

    Getting Cryptocurrencies on Zustand

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,15 @@
+import axios from "axios";
 import { create } from "zustand";
 
+const getCriptos = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+      const { data: { Data } } = await axios(url);
+
+      console.log(Data);
+}
 
 export const useCryptoStore = create(() => ({
       fetchCrypto: () => {
-            console.log('desde FetchCrypto');
+            getCriptos();
       }
 }))
