ed3cbf5666f025ba3656e8b2275e741e5fb47b38
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-18 18:52:08 +0200

    Validating the API response with Zod

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { create } from "zustand";
+import { CrytoCurrenciesResponseShema } from "../shema/cripto-shema";
 
 const getCriptos = async () => {
       const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
       const { data: { Data } } = await axios(url);
-
-      console.log(Data);
+      const result = CrytoCurrenciesResponseShema.safeParse(Data);
+      console.log(result);
 }
 
 export const useCryptoStore = create(() => ({
