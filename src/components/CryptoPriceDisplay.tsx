9a435de2c3dd9118974ff27d6356dc8c33ffa7c4
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-20 22:26:18 +0200

    Adding CSS to the quote result

--- src/components/CryptoPriceDisplay.tsx
+++ src/components/CryptoPriceDisplay.tsx
@@ -5,17 +5,22 @@ const CryptoPriceDisplay = () => {
 
       const { result } = useCryptoStore();
       const hasResult = useMemo(() => !Object.values(result).includes(''), [result]);
-      //console.log(result);
+      // console.log(result.IMAGEURL);
 
       return (
-            <div>
+            <div className="result-wrapper">
                   {
                         hasResult && (
                               <>
                                     <h2>Cotización</h2>
                                     <div className="result">
+                                          <img src={`https://cryptocompare.com/${result.IMAGEURL}`} alt="image cryptomoneda" />
                                           <div className="">
                                                 <p>El precio es de: <span>{result.PRICE}</span></p>
+                                                <p>Precio mas alto del dia : <span>{result.HIGHDAY}</span></p>
+                                                <p>Precio mas bajo del dia: <span>{result.LOWDAY}</span></p>
+                                                <p>Variación últimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span></p>
+                                                <p>Última actualización: <span>{result.LASTUPDATE}</span></p>
                                           </div>
                                     </div>
                               </>
